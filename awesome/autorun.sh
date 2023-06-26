74b1516e075369c92743b31a329d1fed06daff56
Vespertine <hillbgh@gmail.com>
2023-06-25 19:48:20 -0600

    Spring cleaning for the dotfiles

--- awesome/autorun.sh
+++ awesome/autorun.sh
@@ -10,4 +10,9 @@ else
   polybar --reload example &
 fi
 
-exec picom -b --config ~/Extensible/dotfiles/picom.conf
+# Set the refresh rate to 144hz. You may need to change the output target. Figure that out with xrandr
+xrandr --output DP-2 --mode 2560x1440 --rate 144
+xrandr --output DP-4 --mode 2560x1440 --rate 144
+
+exec picom -b --config ~/Extensible/dotfiles/picom/picom.conf
+
--- awesome/rc.lua
+++ awesome/rc.lua
@@ -61,19 +61,19 @@ modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
-    awful.layout.suit.floating,
     awful.layout.suit.tile,
-    awful.layout.suit.tile.left,
-    awful.layout.suit.tile.bottom,
-    awful.layout.suit.tile.top,
-    awful.layout.suit.fair,
-    awful.layout.suit.fair.horizontal,
-    awful.layout.suit.spiral,
     awful.layout.suit.spiral.dwindle,
-    awful.layout.suit.max,
-    awful.layout.suit.max.fullscreen,
-    awful.layout.suit.magnifier,
-    awful.layout.suit.corner.nw,
+    -- awful.layout.suit.floating,
+    -- awful.layout.suit.tile.left,
+    -- awful.layout.suit.tile.bottom,
+    -- awful.layout.suit.tile.top,
+    -- awful.layout.suit.fair,
+    -- awful.layout.suit.spiral,
+    -- awful.layout.suit.max,
+    -- awful.layout.suit.fair.horizontal,
+    -- awful.layout.suit.max.fullscreen,
+    -- awful.layout.suit.magnifier,
+    -- awful.layout.suit.corner.nw,
     -- awful.layout.suit.corner.ne,
     -- awful.layout.suit.corner.sw,
     -- awful.layout.suit.corner.se,
@@ -566,9 +566,9 @@ beautiful.useless_gap = 12
 size_hint_honor = false
 
 -- Enable sloppy focus, so that focus follows mouse.
-client.connect_signal("mouse::enter", function(c)
-    c:emit_signal("request::activate", "mouse_enter", {raise = false})
-end)
+-- client.connect_signal("mouse::enter", function(c)
+--     c:emit_signal("request::activate", "mouse_enter", {raise = false})
+-- end)
 
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
