af5a349c202afaec0dd8e58e269292a5a2ed5a76
vespertine <hillbgh@gmail.com>
2024-02-26 17:21:15 -0700

    Fixing auto installs for lsp stuff

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -69,10 +69,6 @@ return {
 
 			require('mason-lspconfig').setup({
 				ensure_installed = {
-					'tsserver',
-					'eslint',
-					'lua_ls',
-					'rust_analyzer'
 				},
 				handlers = {
 					lsp_zero.default_setup,
