9d8131ff530dd94913b2f3dd11ec5424b1521098
Vespertine <hillbgh@gmail.com>
2024-02-28 12:39:21 -0700

    quick fix

--- nvim/lua/plugins/inc-rename.lua
+++ nvim/lua/plugins/inc-rename.lua
@@ -3,7 +3,7 @@ return {
     config = function()
         require("inc_rename").setup(
             {
-                cmd_name = "IncRename", -- the name of the command
+                cmd_name = "IncRename ", -- the name of the command
                 hl_group = "Substitute", -- the highlight group used for highlighting the identifier's new name
                 preview_empty_name = false, -- whether an empty new name should be previewed; if false the command preview will be cancelled instead
                 show_message = true, -- whether to display a `Renamed m instances in n files` message after a rename operation
