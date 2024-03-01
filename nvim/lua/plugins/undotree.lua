90f6fb5051dc7f1264a504fbe40064e5be7db125
Vespertine <hillbgh@gmail.com>
2024-03-01 12:01:01 -0700

    Updated the undo-tree & notify configs

--- nvim/lua/plugins/undotree.lua
+++ nvim/lua/plugins/undotree.lua
@@ -1,9 +1,15 @@
--- Massively clutch. Force this not to lazy load & set high priority so it builds the tree as soon as files are opened
 return {
-	"mbbill/undotree",
-	lazy = false,
-	priority = 1000,
-	keys = {
-		{ "<leader>u", vim.cmd.UndotreeToggle, desc = "Toggle undo tree" },
+	"jiaoshijie/undotree",
+	dependencies = "nvim-lua/plenary.nvim",
+	opts = {},
+	keys = { -- load the plugin only when using it's keybinding:
+		{
+			"<leader>u",
+			function()
+				require("undotree").toggle()
+			end,
+			desc = "Toggle undo tree",
+		},
 	},
+	name = "undotree.lua",
 }
