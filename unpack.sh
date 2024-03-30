e55b785e75c6267ec66eeff485d373ad80dc2845
Vespertine <hillbgh@gmail.com>
2024-03-30 11:29:48 -0600

    Fixing order of installs

--- unpack.sh
+++ unpack.sh
@@ -1,3 +1,7 @@
+# Setup Git Defaults
+  git config --global user.email "hillbgh@gmail.com"
+  git config --global user.name "Vespertine"
+
 # Install nessecary AUR packs
 aur_packages=(
     brave-bin
@@ -24,16 +28,20 @@ yay -S --noconfirm $(printf "%s " "${aur_packages[@]}")
 tar -xvf ./assets/JetBrainsMono.tar.xz -C ./assets/
 mkdir -p /usr/share/fonts/myfonts
 mv ./assets/*.ttf /usr/share/fonts/myfonts/
+rm ./assets/*.ttf ./assets/*.txt ./assets/*.md
 
 # ======== ZSH ========
+# Install Oh-My-Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Setup Zsh
+chsh -s $(which zsh)
+
 # Zsh-syntax-highlighting for plugin manager (Oh-My-Zsh)
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # Zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-# Install Oh-My-Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Symlink the stuff
 ln -s ~/Extensible/dotfiles/zsh/.zshrc ~/
@@ -41,9 +49,7 @@ ln -s ~/Extensible/dotfiles/alacritty/ ~/.config/
 ln -s ~/Extensible/dotfiles/btop/ ~/.config/
 ln -s ~/Extensible/dotfiles/starship/starship.toml ~/.config/
 ln -s ~/Extensible/dotfiles/lazygit/ ~/.config/
-
-# Setup Zsh
-chsh -s $(which zsh)
+ln -s ~/Extensible/dotfiles/nvim/ ~/.config
 
 # ==== AWESOME INSTALLATION & CONFIG =====
 awesome_config_packages=(
@@ -86,6 +92,7 @@ systemctl --user start mpd.service
 
 git submodule update --init --recursive
 
+# Copies awesomewm & other configs 
 ln -s ./Boru/config/* ~/.config/ 
 
 sudo cp -r ./Boru/misc/fonts/* /usr/share/fonts/myfonts/
