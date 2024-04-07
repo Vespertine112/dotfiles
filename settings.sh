c15c852c90b52caf91fb907e5d820ef1b82f5080
Vespertine <hillbgh@gmail.com>
2024-04-06 21:47:31 -0600

    Small updates

--- settings.sh
+++
@@ -1,3 +0,0 @@
-# Set the refresh rate to 144hz. You may need to change the output target. Figure that out with xrandr
-xrandr --output DP-2 --mode 2560x1440 --rate 144
-xrandr --output DP-4 --mode 2560x1440 --rate 144
