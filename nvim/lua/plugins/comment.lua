58255e7075c40eb041e6959b83883a1c7ec00c10
Vespertine <hillbgh@gmail.com>
2026-03-10 22:53:31 -0400

    Gupdates

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -1,9 +1,29 @@
 -- Comment functionality
+-- return {
+-- 	"numToStr/Comment.nvim",
+-- 	opts = {
+-- 		toggler = { line = "", block = "gbc" },
+-- 		opleader = { line = "" },
+-- 	},
+-- 	lazy = false,
+-- }
+
 return {
-	"numToStr/Comment.nvim",
-	opts = {
-		toggler = { line = "", block = "gbc" },
-		opleader = { line = "" },
-	},
-	lazy = false,
+    "numToStr/Comment.nvim",
+    lazy = false,
+    config = function()
+        require("Comment").setup({
+            -- Remove toggler opts; we handle keymaps manually below
+            opleader = { block = "gb" },
+        })
+
+        local api = require("Comment.api")
+
+        -- Normal mode: toggle current line
+        -- <C-_> = what Mac/Linux terminals send for Ctrl+/
+        -- <C-/> = what Windows/kitty-protocol terminals send
+        vim.keymap.set("n", "<C-_>", api.toggle.linewise.current, { desc = "Toggle comment" })
+        vim.keymap.set("n", "<C-/>", api.toggle.linewise.current, { desc = "Toggle comment" })
+
+    end,
 }
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -41,6 +41,7 @@ plugins=(
 )
 
 source $ZSH/oh-my-zsh.sh
+source /usr/share/nvm/init-nvm.sh
 source $ZSH/plugins/zsh-interactive-cd/zsh-interactive-cd.plugin.zsh
 
 # User configuration
